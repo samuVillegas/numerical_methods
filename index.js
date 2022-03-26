1bdf54654b563850a5739aa9c625bcafd1f0b6dd
samuVillegas <villegassamuel25@gmail.com>
2022-03-26 07:50:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// F(x) = 0
+const busquedasInc = require('./Functions/busquedasInc')
+const biseccion = require('./Functions/biseccion')
+const puntoFijo = require('./Functions/puntoFijo')
+const newton = require('./Functions/newton')
+const secante = require('./Functions/secante')
+const raicesMultiples = require('./Functions/raicesMultiples')
+
+
+// Ax=b
+const gausSimple = require('./Matrixes/gausSimple')
+const gausPivoteoParcial = require('./Matrixes/gausPivoteoParcial')
+const gausPivoteoTotal = require('./Matrixes/gausPivoteoTotal')
+
+
+//Main
+
