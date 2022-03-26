60e7589fd49c8534bc933fc7a56ed11957833415
samuVillegas <villegassamuel25@gmail.com>
2022-03-26 14:32:46 -0500

    Se realiza método punto dijo

--- Functions/puntoFijo.js
+++ Functions/puntoFijo.js
@@ -1,3 +1,26 @@
-module.exports = () => {
-    console.log('Hola mundo')
+const mathjs = require('mathjs')
+
+module.exports = (fx,gx,x0,tolerance,nMax) => {
+    const iterations = []
+    fxi = mathjs.evaluate(fx,{x:x0});
+    if (fxi === 0) {
+        return { state: 'Success', root: x0 }
+    }else {
+        counter = 0;
+        xi = x0
+        iterations.push({counter,xi,fxi});
+        error = tolerance + 1;
+        while(fxi !== 0 && error > tolerance && counter < nMax){
+            xaux = xi;
+            xi = mathjs.evaluate(gx,{x:xaux});
+            fx = mathjs.evaluate(fx,{x:xi});
+            error = Math.abs(xi-xaux);
+            counter++;
+            iterations.push({counter,xi,fxi,error});
+        }
+        if(fxi === 0) return {state: 'Success', iterations, root: xi}
+        if(error <= tolerance) return { state: 'Success', iterations, approximation: xi}
+        return { state: 'Error', iterations, message: 'The method fails with the maximum number of iterations given'}
+    }
+
 }
--- index.js
+++ index.js
@@ -21,4 +21,6 @@ const gausPivoteoTotal = require('./Matrixes/gausPivoteoTotal')
 //Main
 
 //console.table(busquedasInc('e^(3*x-12)+x*cos(3x)-x^2+1',-10,1,10).iterations)
-console.table(biseccion('log((sin(x)^2 + 1)) - 1/2',0,1,10e-7,100).approximation)
+//console.table(biseccion('log((sin(x)^2 + 1)) - 1/2',0,1,10e-7,100).approximation)
+console.table(puntoFijo('log((sin(x)^2)+1)-x-1/2','log((sin(x)^2)+1)-1/2',-0.5,10e-7,100).iterations)
+
