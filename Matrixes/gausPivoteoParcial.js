47fb82512b7fcceee40479bb069c1e5b93931533
samuVillegas <villegassamuel25@gmail.com>
2022-03-27 21:33:41 -0500

    Se realiza avance en el método de gaus simple, falta la sustitucion regresiva

--- Matrixes/gausPivoteoParcial.js
+++ Matrixes/gausPivoteoParcial.js
@@ -1,3 +1,23 @@
-module.exports = () => {
-    console.log('Hola mundo')
+const mathjs = require('mathjs')
+
+const upperTriangular = (A, b, n) => {
+    m = mathjs.concat(A, b);
+    for (let i = 0; i < n - 1; i++) {
+        if (m[i][i] === 0) break;
+        for (let j = i + 1; j < n; j++) {
+            if (m[j][i] !== 0) {
+                m[j] = mathjs.subtract(mathjs.row(m, j), mathjs.multiply(mathjs.row(m, i), m[j][i] / m[i][i]))[0];
+            }
+        }
+    }
+    return m;
 }
+
+const sustitution = () => {
+    
+}
+
+module.exports = (A, b, n) => {
+    return upperTriangular(A,b,n);
+}
+
--- index.js
+++ index.js
@@ -22,10 +22,13 @@ const gausPivoteoTotal = require('./Matrixes/gausPivoteoTotal')
 
 //Main
 
-console.table(busquedasInc('e^(3*x-12)+x*cos(3x)-x^2+1',-10,1,10).iterations)
-console.table(biseccion('log((sin(x)^2)+ 1)-1/2',0,1,10e-7,100).iterations)
-console.table(reglaFalsa('log((sin(x)^2)+ 1)-1/2',0,1,10e-7,100).iterations)
-console.table(puntoFijo('log((sin(x)^2)+1)-x-1/2','log((sin(x)^2)+1)-1/2',-0.5,10e-7,100).iterations)
-console.table(newton('log((sin(x)^2)+ 1)-1/2','2/(((sin(x)^2)+1))*sin(x)*cos(x)',0.5,10e-7,100).iterations)
-console.table(secante('log((sin(x)^2)+ 1)-1/2',0.5,1,10e-7,100).iterations)
-console.table(raicesMultiples('e^x-x-1','e^x-1','e^x',1,10e-7,100).iterations)
+// console.table(busquedasInc('e^(3*x-12)+x*cos(3x)-x^2+1',-10,1,10).iterations)
+// console.table(biseccion('log((sin(x)^2)+ 1)-1/2',0,1,10e-7,100).iterations)
+// console.table(reglaFalsa('log((sin(x)^2)+ 1)-1/2',0,1,10e-7,100).iterations)
+// console.table(puntoFijo('log((sin(x)^2)+1)-x-1/2','log((sin(x)^2)+1)-1/2',-0.5,10e-7,100).iterations)
+// console.table(newton('log((sin(x)^2)+ 1)-1/2','2/(((sin(x)^2)+1))*sin(x)*cos(x)',0.5,10e-7,100).iterations)
+// console.table(secante('log((sin(x)^2)+ 1)-1/2',0.5,1,10e-7,100).iterations)
+// console.table(raicesMultiples('e^x-x-1','e^x-1','e^x',1,10e-7,100).iterations)
+
+
+gausPivoteoParcial([[14,6,-2,3],[3,15,2,-5],[-7,4,-23,2],[1,-3,-2,16]],[[12],[32],[-24],[14]],4)
